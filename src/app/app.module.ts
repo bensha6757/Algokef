02f2ce27f863f3ab762a5968180728875d776eb0
guy.bagi <guy.bagi@algosec.com>
2021-11-11 03:16:26 +0200

    microsoft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SideNavContentComponent } from './components/side-nav-content/side-nav-content.component';
-import { AppPageComponent } from './pages/app-page/app-page.component';
-import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
-import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { PublicPageComponent } from './public-page/public-page.component';
+import { RestrictedPageComponent } from './restricted-page/restricted-page.component';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '18f0c7f4-9a4d-4e6a-830c-70d405b7e18f',
+      redirectUri: 'http://localhost:4200'
+    }
+  });
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent,
-    HeaderComponent,
-    SideNavContentComponent,
-    AppPageComponent,
-    PortfolioPageComponent,
-    ServicesPageComponent
+    PublicPageComponent,
+    RestrictedPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
